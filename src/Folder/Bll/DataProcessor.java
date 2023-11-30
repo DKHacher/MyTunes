fde7f70d504436c24545d12d8869be46e222f00d
DKHacher <146067965+DKHacher@users.noreply.github.com>
2023-11-30 09:36:40 +0100

    made the example a txt file and removed unnecesary code

--- src/Folder/Bll/DataProcessor.java
+++ src/Folder/Bll/DataProcessor.java
@@ -1,12 +1,5 @@
 package Folder.Bll;
 
 public class DataProcessor {
-    public String personalizedMessage(String age, String name){
-        if (Integer.parseInt(age) > 0){
-            return "Greetings " + name + " and may i bid you good day";
-        }
-        else{
-            return "the age you have entered is not a valid number";
-        }
-    }
+
 }
--- src/Folder/Gui/MainController.java
+++ src/Folder/Gui/MainController.java
@@ -8,19 +8,5 @@ import javafx.scene.control.TextField;
 import Folder.Bll.DataProcessor;
 
 public class MainController {
-    @FXML
-    private Label messagelbl;
-    @FXML
-    private Button greetingsbtn;
-    @FXML
-    private TextField nametxt;
-    @FXML
-    private TextField agetxt;
-    private final DataProcessor dataProcessor = new DataProcessor();
 
-
-    public void displayGreetingsMessage(ActionEvent actionEvent) {
-        messagelbl.setText(dataProcessor.personalizedMessage(agetxt.getText(), nametxt.getText()));
-
-    }
 }
